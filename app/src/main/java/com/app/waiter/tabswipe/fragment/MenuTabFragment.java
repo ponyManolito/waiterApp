6bf177468477ff2693acb8ee4d3008ece5a32494
Javier Gomez <javier.gomez@eurotaxglass.com>
2015-05-27 11:22:40 +0200

    First approach to Menu Tab. Now showing header and items.

--- app/src/main/java/com/app/waiter/tabswipe/fragment/MenuTabFragment.java
+++ app/src/main/java/com/app/waiter/tabswipe/fragment/MenuTabFragment.java
@@ -1,6 +1,7 @@
 package com.app.waiter.tabswipe.fragment;
 
 import android.app.ListFragment;
+import android.content.Context;
 import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,12 +9,19 @@ import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.waiter.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by javier.gomez on 13/05/2015.
@@ -34,12 +42,32 @@ public class MenuTabFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_menu_waiter, null);
         View insideListView = inflater.inflate(R.layout.list_menu_waiter, null);
         View headerListView = inflater.inflate(R.layout.list_menu_header_waiter, null);
+
         TextView tv = (TextView) view.findViewById(R.id.textViewTitle);
         tv.setText(tab);
 
-        TextView textViewSeparator = (TextView) headerListView.findViewById(R.id.itemSeparator);
-        textViewSeparator.setText("Primeros");
+        // SQL query, then FOR state
+        createHeaders(headerListView, view);
+        createList(insideListView, view);
 
+        return view;
+    }
+
+    private void createHeaders(View headerView, View view) {
+        String[] columns = new String[] {"_id", "head"};
+        MatrixCursor cursor = new MatrixCursor(columns);
+        cursor.addRow(new Object[] {"0", "Primeros"});
+        String[] fromColumns = {"head"};
+        int[] toViews = {R.id.itemSeparator};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(headerView.getContext(),
+                R.layout.list_menu_header_waiter, cursor, fromColumns, toViews, 0);
+
+        ListView listViewHeader = (ListView) view.findViewById(R.id.listViewMenuHeader);
+        listViewHeader.setAdapter(adapter);
+    }
+
+    private void createList(View insideView, View view) {
         String[] columns = new String[] {"_id", "name", "price"};
         MatrixCursor cursor = new MatrixCursor(columns);
         cursor.addRow(new Object[] {"0","Pollo","10.50 €"});
@@ -53,21 +81,10 @@ public class MenuTabFragment extends Fragment {
         cursor.addRow(new Object[] {"8","Pollo","10.50 €"});
         cursor.addRow(new Object[] {"9","Pollo","10.50 €"});
         cursor.addRow(new Object[] {"10","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"11","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"12","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"13","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"14","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"15","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"16","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"17","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"18","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"19","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"20","Pollo","10.50 €"});
-        cursor.addRow(new Object[] {"21","Pollo","10.50 €"});
 
         String[] fromColumns = {"name", "price"};
         int[] toViews = {R.id.foodTitle, R.id.foodPrice};
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(insideListView.getContext(),
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(insideView.getContext(),
                 R.layout.list_menu_waiter, cursor, fromColumns, toViews, 0);
 
         ListView listView = (ListView) view.findViewById(R.id.listViewMenu);
@@ -80,8 +97,6 @@ public class MenuTabFragment extends Fragment {
                 Toast.makeText(view.getContext(), text, Toast.LENGTH_LONG).show();
             }
         });
-
-        return view;
     }
 
 
